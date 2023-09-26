5f2cb899a6570fc5923937156d5ac878fcd0e1eb
kenlau5678 <63826053+kenlau5678@users.noreply.github.com>
2023-09-26 16:30:38 +0800

    Add files via upload

--- js/1.js
+++ js/1.js
@@ -3,6 +3,7 @@ document.addEventListener('touchstart',function (ev) {
     startY = ev.touches[0].pageY;
 }, false);
 
+var content = document.querySelectorAll(".content")
 document.addEventListener('touchend',function (ev) {
     var endY;//定义结束时候的坐标位置
     endY = ev.changedTouches[0].pageY;
@@ -14,8 +15,8 @@ document.addEventListener('touchend',function (ev) {
         case 1:
              console.log("  向上");
              
-             var content = document.querySelector(".content")
-            content.style.display = "block"
+             
+            content[0].style.display = "block"
             break;
         case 2:
              console.log("向下");
@@ -25,6 +26,22 @@ document.addEventListener('touchend',function (ev) {
     }
   }, false);
 
+var buttons = document.querySelectorAll(".buttons")
+buttons[0].onclick = function(){
+    content[0].style.display = "none"
+    content[1].style.display = "block"
+}
+
+var con = document.querySelector(".continue")
+con.onclick = function(){
+    content[1].style.display = "none"
+    content[2].style.display = "block"
+}
+
+var end = document.querySelector(".end")
+end.onclick = function(){
+    window.history.go(-1);
+}
 function getDirection(startY, endY) { //根据坐标判断是上拉还是下滑
     var dy = startY - endY;
     var result = 0;
