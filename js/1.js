bb81f2ca396af9a0702f07ce4e17c8041539d22a
kenlau5678 <63826053+kenlau5678@users.noreply.github.com>
2023-10-02 23:16:57 +0800

    Add files via upload

--- js/1.js
+++ js/1.js
@@ -40,7 +40,7 @@ con.onclick = function(){
 
 var end = document.querySelector(".end")
 end.onclick = function(){
-    history.go(-1);
+    window.location.replace(document.referrer);
 }
 
 function getDirection(startY, endY) { //根据坐标判断是上拉还是下滑
